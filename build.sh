b73a188c486a8788cb553e5e61071d82d915c1d6
Özgür Akgün <ozgurakgun@gmail.com>
2022-02-09 12:14:56 +0000

    updating the papers

--- build.sh
+++ build.sh
@@ -3,9 +3,10 @@
 set -o errexit
 set -o nounset
 
+bibtex-clean < bib/google.bib > bib/google-clean.bib ; mv bib/google-clean.bib bib/google.bib
 bibtex-clean < bib/pure.bib > bib/pure-clean.bib ; mv bib/pure-clean.bib bib/pure.bib
 ruby bib/render.rb > _includes/bib.html
 
-bundle install
+bundle install --path vendor/bundle
 bundle exec jekyll build
 bundle exec htmlproofer _site --assume-extension --check-html --disable-external
